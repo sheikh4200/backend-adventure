205fbcd0421cd1c88995f0c24095c92037304451
sheikh4200 <159758352+sheikh4200@users.noreply.github.com>
2024-11-15 11:32:36 +0500

    backend

---
+++ index.js
@@ -0,0 +1,31 @@
+// 1. Import the required modules
+import express from "express"
+// const express = require('express');
+
+const app = express();
+const port = 4000
+
+app.get("/",(req,res)=>{
+    res.send("Hello, World!");
+})
+app.get("/facebook",(req,res)=>{
+    res.send("Welcome to Facebook!");
+})
+app.get ("/youtube",(req,res)=>{
+res.send("welcome to youtube")
+})
+
+
+
+// 2. Start the server 
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+
+
+
+export {app}
+
+
